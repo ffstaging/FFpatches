FFmpeg/FFmpeg

commit 2d0bfbd0fafe5e869919120758903801f91530fa

fftools/cmdutils: Use av_dynarray_add_nofree()
Simplifies code and reduces the number of allocations a bit
by overallocating.

Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

fftools/cmdutils.c
@@ -2216,19 +2216,13 @@ void *grow_array(void *array, int elem_size, int *size, int new_size)
 
 void *allocate_array_elem(void *ptr, size_t elem_size, int *nb_elems)
 {
-    void *new_elem, **array;
+    void *new_elem;
 
-    memcpy(&array, ptr, sizeof(array));
-    if (*nb_elems == INT_MAX) {
-        av_log(NULL, AV_LOG_ERROR, "Array too big.\n");
+    if (!(new_elem = av_mallocz(elem_size)) ||
+        av_dynarray_add_nofree(ptr, nb_elems, new_elem) < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Could not alloc buffer.\n");
         exit_program(1);
     }
-    new_elem = av_mallocz(elem_size);
-    if (!new_elem)
-        exit_program(1);
-    GROW_ARRAY(array, *nb_elems);
-    memcpy(ptr, &array, sizeof(array));
-    array[*nb_elems - 1] = new_elem;
     return new_elem;
 }
 

ffstaging/FFpatches

commit b91ead4e82add9ba0735982a08afad1b8329a6c6

fftools/cmdutils: Add av_freep() to avoid memory leak
Add av_freep() to free new_elem to avoid memory leak.

Fixes: 2d0bfbd0fa ("fftools/cmdutils: Use av_dynarray_add_nofree()")
Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>

fftools/cmdutils.c
@@ -1471,9 +1471,13 @@ void *allocate_array_elem(void *ptr, size_t elem_size, int *nb_elems)
 {
     void *new_elem;
 
-    if (!(new_elem = av_mallocz(elem_size)) ||
-        av_dynarray_add_nofree(ptr, nb_elems, new_elem) < 0)
+    new_elem = av_mallocz(elem_size);
+    if (!new_elem)
         return NULL;
+    if (av_dynarray_add_nofree(ptr, nb_elems, new_elem) < 0) {
+        av_freep(&new_elem);
+        return NULL;
+    }
     return new_elem;
 }
 