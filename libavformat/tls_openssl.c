6948a69d18a79be37975b57191794c0830aac63b
Jack Lau <jacklau1222gm@gmail.com>
2025-07-13 15:09:50 +0800

    avformat/tls_openssl: init DTLS context with explicit method
    Signed-off-by: Jack Lau <jacklau1222@qq.com>

--- libavformat/tls_openssl.c
+++ libavformat/tls_openssl.c
@@ -787,7 +787,7 @@ static int dtls_start(URLContext *h, const char *url, int flags, AVDictionary **
     /* Refer to the test cases regarding these curves in the WebRTC code. */
     const char* curves = "X25519:P-256:P-384:P-521";
 
-    p->ctx = SSL_CTX_new(DTLS_method());
+    p->ctx = SSL_CTX_new(c->listen ? DTLS_server_method() : DTLS_client_method());
     if (!p->ctx) {
         ret = AVERROR(ENOMEM);
         goto fail;
