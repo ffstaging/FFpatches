FFmpeg/FFmpeg

commit 9a38184a143a1560814b084aebe628f8df46e666

examples/decode_audio: allocate the packet dynamically
AVPackets on stack are discouraged now.

doc/examples/decode_audio.c
@@ -97,7 +97,7 @@ int main(int argc, char **argv)
     uint8_t inbuf[AUDIO_INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
     uint8_t *data;
     size_t   data_size;
-    AVPacket avpkt;
+    AVPacket *pkt;
     AVFrame *decoded_frame = NULL;
 
     if (argc <= 2) {
@@ -110,7 +110,7 @@ int main(int argc, char **argv)
     /* register all the codecs */
     avcodec_register_all();
 
-    av_init_packet(&avpkt);
+    pkt = av_packet_alloc();
 
     /* find the MPEG audio decoder */
     codec = avcodec_find_decoder(AV_CODEC_ID_MP2);
@@ -156,7 +156,7 @@ int main(int argc, char **argv)
             }
         }
 
-        ret = av_parser_parse2(parser, c, &avpkt.data, &avpkt.size,
+        ret = av_parser_parse2(parser, c, &pkt->data, &pkt->size,
                                data, data_size,
                                AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
         if (ret < 0) {
@@ -166,8 +166,8 @@ int main(int argc, char **argv)
         data      += ret;
         data_size -= ret;
 
-        if (avpkt.size)
-            decode(c, &avpkt, decoded_frame, outfile);
+        if (pkt->size)
+            decode(c, pkt, decoded_frame, outfile);
 
         if (data_size < AUDIO_REFILL_THRESH) {
             memmove(inbuf, data, data_size);
@@ -185,6 +185,7 @@ int main(int argc, char **argv)
     avcodec_free_context(&c);
     av_parser_close(parser);
     av_frame_free(&decoded_frame);
+    av_packet_free(&pkt);
 
     return 0;
 }

ffstaging/FFpatches

commit d8c793a940241f0a90e3b5543dbbe84def9667d0

examples: Replace with proper error handling to avoid potential memory errors
Add check for the return valud of av_packet_alloc() to avoid potential NULL pointer dereference.
Moreover, replace with proper error handling to avoid potential memory leak.

Fixes: 9a38184a14 ("examples/decode_audio: allocate the packet dynamically")
Fixes: f5df897c4b ("examples/avcodec: split audio decoding into a separate example")
Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>

doc/examples/decode_audio.c
@@ -128,41 +128,48 @@ int main(int argc, char **argv)
     outfilename = argv[2];
 
     pkt = av_packet_alloc();
+    if (!pkt)
+        exit(1);
 
     /* find the MPEG audio decoder */
     codec = avcodec_find_decoder(AV_CODEC_ID_MP2);
     if (!codec) {
         fprintf(stderr, "Codec not found\n");
-        exit(1);
+        ret = -1;
+        goto free_pkt;
     }
 
     parser = av_parser_init(codec->id);
     if (!parser) {
         fprintf(stderr, "Parser not found\n");
-        exit(1);
+        ret = -1;
+        goto free_pkt;
     }
 
     c = avcodec_alloc_context3(codec);
     if (!c) {
         fprintf(stderr, "Could not allocate audio codec context\n");
-        exit(1);
+        ret = -1;
+        goto close_parser;
     }
 
     /* open it */
     if (avcodec_open2(c, codec, NULL) < 0) {
         fprintf(stderr, "Could not open codec\n");
-        exit(1);
+        ret = -1;
+        goto free_context;
     }
 
     f = fopen(filename, "rb");
     if (!f) {
         fprintf(stderr, "Could not open %s\n", filename);
-        exit(1);
+        ret = -1;
+        goto free_context;
     }
     outfile = fopen(outfilename, "wb");
     if (!outfile) {
-        av_free(c);
-        exit(1);
+        ret = -1;
+        goto fclose_f;
     }
 
     /* decode until eof */
@@ -173,7 +180,8 @@ int main(int argc, char **argv)
         if (!decoded_frame) {
             if (!(decoded_frame = av_frame_alloc())) {
                 fprintf(stderr, "Could not allocate audio frame\n");
-                exit(1);
+                ret = -1;
+                goto fclose_outfile;
             }
         }
 
@@ -182,7 +190,7 @@ int main(int argc, char **argv)
                                AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
         if (ret < 0) {
             fprintf(stderr, "Error while parsing\n");
-            exit(1);
+            goto end;
         }
         data      += ret;
         data_size -= ret;
@@ -225,13 +233,20 @@ int main(int argc, char **argv)
            fmt, n_channels, c->sample_rate,
            outfilename);
 end:
-    fclose(outfile);
-    fclose(f);
-
-    avcodec_free_context(&c);
-    av_parser_close(parser);
     av_frame_free(&decoded_frame);
+fclose_outfile:
+    fclose(outfile);
+fclose_f:
+    fclose(f);
+free_context:
+    avcodec_free_context(&c);
+close_parser:
+    av_parser_close(parser);
+free_pkt:
     av_packet_free(&pkt);
+
+    if (ret < 0)
+        exit(1);
 
     return 0;
 }