FFmpeg/FFmpeg

commit 9a38184a143a1560814b084aebe628f8df46e666

examples/decode_audio: allocate the packet dynamically
AVPackets on stack are discouraged now.

doc/examples/decode_audio.c
@@ -97,7 +97,7 @@ int main(int argc, char **argv)
     uint8_t inbuf[AUDIO_INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
     uint8_t *data;
     size_t   data_size;
-    AVPacket avpkt;
+    AVPacket *pkt;
     AVFrame *decoded_frame = NULL;
 
     if (argc <= 2) {
@@ -110,7 +110,7 @@ int main(int argc, char **argv)
     /* register all the codecs */
     avcodec_register_all();
 
-    av_init_packet(&avpkt);
+    pkt = av_packet_alloc();
 
     /* find the MPEG audio decoder */
     codec = avcodec_find_decoder(AV_CODEC_ID_MP2);
@@ -156,7 +156,7 @@ int main(int argc, char **argv)
             }
         }
 
-        ret = av_parser_parse2(parser, c, &avpkt.data, &avpkt.size,
+        ret = av_parser_parse2(parser, c, &pkt->data, &pkt->size,
                                data, data_size,
                                AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
         if (ret < 0) {
@@ -166,8 +166,8 @@ int main(int argc, char **argv)
         data      += ret;
         data_size -= ret;
 
-        if (avpkt.size)
-            decode(c, &avpkt, decoded_frame, outfile);
+        if (pkt->size)
+            decode(c, pkt, decoded_frame, outfile);
 
         if (data_size < AUDIO_REFILL_THRESH) {
             memmove(inbuf, data, data_size);
@@ -185,6 +185,7 @@ int main(int argc, char **argv)
     avcodec_free_context(&c);
     av_parser_close(parser);
     av_frame_free(&decoded_frame);
+    av_packet_free(&pkt);
 
     return 0;
 }

ffstaging/FFpatches

commit 87104e06d23dddbc771df5a1eb6bc5c4c08744d0

examples: Add check and replace av_free() to avoid potential memory errors
Add check for the return value of av_packet_alloc() to avoid potential NULL pointer dereference.
Moreover, replace redundant av_free() with fprintf().

Fixes: 9a38184a14 ("examples/decode_audio: allocate the packet dynamically")
Fixes: f5df897c4b ("examples/avcodec: split audio decoding into a separate example")
Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>

doc/examples/decode_audio.c
@@ -128,6 +128,10 @@ int main(int argc, char **argv)
     outfilename = argv[2];
 
     pkt = av_packet_alloc();
+    if (!pkt) {
+        fprintf(stderr, "Could not allocate AVPacket\n");
+        exit(1); /* or proper cleanup and returning */
+    }
 
     /* find the MPEG audio decoder */
     codec = avcodec_find_decoder(AV_CODEC_ID_MP2);
@@ -161,7 +165,7 @@ int main(int argc, char **argv)
     }
     outfile = fopen(outfilename, "wb");
     if (!outfile) {
-        av_free(c);
+        fprintf(stderr, "Could not open %s\n", outfilename);
         exit(1);
     }
 