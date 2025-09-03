14ecd7c143f6c2779ddd3831159a1fad081c80e6
Jamaika1 <lukaszcz18@wp.pl>
2025-09-03 13:40:51 +0000

    Changing vulkan file directory

---
+++ libavcodec/vulkan/vulkan_source.h
@@ -0,0 +1,35 @@
+/*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef AVCODEC_VULKAN_SOURCE_H
+#define AVCODEC_VULKAN_SOURCE_H
+
+extern const char *ff_source_common_comp;
+extern const char *ff_source_ffv1_common_comp;
+extern const char *ff_source_ffv1_dec_comp;
+extern const char *ff_source_ffv1_dec_setup_comp;
+extern const char *ff_source_ffv1_enc_comp;
+extern const char *ff_source_ffv1_enc_rct_comp;
+extern const char *ff_source_ffv1_enc_setup_comp;
+extern const char *ff_source_ffv1_rct_comp;
+extern const char *ff_source_ffv1_rct_search_comp;
+extern const char *ff_source_ffv1_reset_comp;
+extern const char *ff_source_ffv1_vlc_comp;
+extern const char *ff_source_rangecoder_comp;
+
+#endif /* AVCODEC_VULKAN_SOURCE_H */
