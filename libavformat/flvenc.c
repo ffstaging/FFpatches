b07b8404d02445ccaeb660a0019f7847e8177345
jiangjie <code@ffmpeg.org>
2025-09-12 02:58:22 +0000

    avformat/flvenc: event_flags is a variable of AVStream (PR #20499)
    PR #20499 opened by jiangjie
    URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/20499
    Patch URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/20499.patch
    
    event_flags is a variable of AVStream, not AVFormatContext
    
    >From f95b2eb729ec868796c9a65a9c8ce76b5f660a84 Mon Sep 17 00:00:00 2001
    From: jiangjie <jiangjie618@gmail.com>
    Date: Fri, 12 Sep 2025 10:49:19 +0800
    Subject: [PATCH] avformat/flvenc: event_flags is a variable of AVStream

--- libavformat/flvenc.c
+++ libavformat/flvenc.c
@@ -1270,9 +1270,9 @@ static int flv_write_packet(AVFormatContext *s, AVPacket *pkt)
 
     ts = pkt->dts;
 
-    if (s->event_flags & AVSTREAM_EVENT_FLAG_METADATA_UPDATED) {
+    if (s->streams[pkt->stream_index]->event_flags & AVSTREAM_EVENT_FLAG_METADATA_UPDATED) {
         write_metadata(s, ts);
-        s->event_flags &= ~AVSTREAM_EVENT_FLAG_METADATA_UPDATED;
+        s->streams[pkt->stream_index]->event_flags &= ~AVSTREAM_EVENT_FLAG_METADATA_UPDATED;
     }
 
     avio_write_marker(pb, av_rescale(ts, AV_TIME_BASE, 1000),
