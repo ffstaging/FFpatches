FFmpeg/FFmpeg

commit afad332d77278232662e6d8c95c2fc80cb44c8dd

avutil/attributes: use __has_attribute to test for format attribute
Signed-off-by: Kacper Michajłow <kasper93@gmail.com>

libavutil/attributes.h
@@ -183,13 +183,13 @@
 
 #ifdef __MINGW_PRINTF_FORMAT
 #    define AV_PRINTF_FMT __MINGW_PRINTF_FORMAT
-#elif defined(__GNUC__) || defined(__clang__)
+#elif defined(__has_attribute) && __has_attribute(format)
 #    define AV_PRINTF_FMT __printf__
 #endif
 
 #ifdef __MINGW_SCANF_FORMAT
 #    define AV_SCANF_FMT __MINGW_SCANF_FORMAT
-#elif defined(__GNUC__) || defined(__clang__)
+#elif defined(__has_attribute) && __has_attribute(format)
 #    define AV_SCANF_FMT __scanf__
 #endif
 

ffstaging/FFpatches

commit f4c58258e50193cb9ccab98b156103b07a920de8

avutil/attributes: add AV_HAS_ATTRIBUTE
Maybe useful and to fix MSVC preprocessor warnings.

Fixes: afad332d77278232662e6d8c95c2fc80cb44c8dd
Signed-off-by: Kacper Michajłow <kasper93@gmail.com>

libavutil/attributes.h
@@ -40,6 +40,12 @@
 #    define AV_HAS_BUILTIN(x) 0
 #endif
 
+#ifdef __has_attribute
+#    define AV_HAS_ATTRIBUTE(x) __has_attribute(x)
+#else
+#    define AV_HAS_ATTRIBUTE(x) 0
+#endif
+
 #if defined(__cplusplus) && defined(__has_cpp_attribute)
 #    define AV_HAS_STD_ATTRIBUTE(x) __has_cpp_attribute(x)
 #elif !defined(__cplusplus) && defined(__has_c_attribute)
@@ -183,13 +189,13 @@
 
 #ifdef __MINGW_PRINTF_FORMAT
 #    define AV_PRINTF_FMT __MINGW_PRINTF_FORMAT
-#elif defined(__has_attribute) && __has_attribute(format)
+#elif AV_HAS_ATTRIBUTE(format)
 #    define AV_PRINTF_FMT __printf__
 #endif
 
 #ifdef __MINGW_SCANF_FORMAT
 #    define AV_SCANF_FMT __MINGW_SCANF_FORMAT
-#elif defined(__has_attribute) && __has_attribute(format)
+#elif AV_HAS_ATTRIBUTE(format)
 #    define AV_SCANF_FMT __scanf__
 #endif
 