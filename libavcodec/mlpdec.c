87d73afb4a605e574d08129f8ee1e6878d237d21
James Almer <code@ffmpeg.org>
2025-10-27 14:17:13 +0000

    avcodec/mlpdec: don't depend on context channel layout when setting substream masks (PR #20766)
    PR #20766 opened by James Almer (jamrial)
    URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/20766
    Patch URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/20766.patch
    
    If avctx->ch_layout is unset (as it's allowed and even expeced by the AV_CODEC_CAP_CHANNEL_CONF flag), the code setting substream masks will fail for stereo and mono layouts unless a downmix channel was requested.
    Fix this by deriving the mask with coded values only.
    
    Fixes issue #20764.
    
    >From 44d78ee4961b2f4f751ea902c976fd11335dc378 Mon Sep 17 00:00:00 2001
    From: James Almer <jamrial@gmail.com>
    Date: Mon, 27 Oct 2025 11:13:20 -0300
    Subject: [PATCH] avcodec/mlpdec: don't depend on context channel layout when
     setting substream masks
    
    If avctx->ch_layout is unset (as it's allowed and even expeced by the
    AV_CODEC_CAP_CHANNEL_CONF flag), the code setting substream masks will fail for
    stereo and mono layouts unless a downmix channel was requested.
    Fix this by deriving the mask with coded values only.
    
    Fixes issue #20764.
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/mlpdec.c
+++ libavcodec/mlpdec.c
@@ -453,15 +453,13 @@ static int read_major_sync(MLPDecodeContext *m, GetBitContext *gb)
         }
         m->substream[1].mask = mh.channel_layout_thd_stream1;
         if (mh.channels_thd_stream1 == 2 &&
-            mh.channels_thd_stream2 == 2 &&
-            m->avctx->ch_layout.nb_channels == 2)
+            mh.channels_thd_stream2 == 2)
             m->substream[0].mask = AV_CH_LAYOUT_STEREO;
         if ((substr = (mh.num_substreams > 1)))
             m->substream[0].mask = AV_CH_LAYOUT_STEREO;
         if (mh.num_substreams == 1 &&
             mh.channels_thd_stream1 == 1 &&
-            mh.channels_thd_stream2 == 1 &&
-            m->avctx->ch_layout.nb_channels == 1)
+            mh.channels_thd_stream2 == 1)
             m->substream[0].mask = AV_CH_LAYOUT_MONO;
         if (mh.num_substreams > 2)
             if (mh.channel_layout_thd_stream2)
