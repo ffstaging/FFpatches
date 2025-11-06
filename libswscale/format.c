FFmpeg/FFmpeg

commit a5be0ecbfdab24632a10c8a6714844530f76d368

swscale: add support for 10/12-bit grayscale MSB pixfmts

libswscale/format.c
@@ -91,6 +91,10 @@ static const LegacyFormatEntry legacy_format_entries[] = {
     [AV_PIX_FMT_GRAY14LE]       = { 1, 1 },
     [AV_PIX_FMT_GRAY16BE]       = { 1, 1 },
     [AV_PIX_FMT_GRAY16LE]       = { 1, 1 },
+    [AV_PIX_FMT_GRAY10MSBBE]    = { 1, 1 },
+    [AV_PIX_FMT_GRAY10MSBLE]    = { 1, 1 },
+    [AV_PIX_FMT_GRAY12MSBBE]    = { 1, 1 },
+    [AV_PIX_FMT_GRAY12MSBLE]    = { 1, 1 },
     [AV_PIX_FMT_YUV440P]        = { 1, 1 },
     [AV_PIX_FMT_YUVJ440P]       = { 1, 1 },
     [AV_PIX_FMT_YUV440P10LE]    = { 1, 1 },
@@ -979,6 +983,10 @@ static int fmt_read_write(enum AVPixelFormat fmt, SwsReadWriteOp *rw_op,
     case AV_PIX_FMT_GRAY14LE:
     case AV_PIX_FMT_GRAY16BE:
     case AV_PIX_FMT_GRAY16LE:
+    case AV_PIX_FMT_GRAY10MSBBE:
+    case AV_PIX_FMT_GRAY10MSBLE:
+    case AV_PIX_FMT_GRAY12MSBBE:
+    case AV_PIX_FMT_GRAY12MSBLE:
     //case AV_PIX_FMT_GRAYF16BE: TODO
     //case AV_PIX_FMT_GRAYF16LE:
     //case AV_PIX_FMT_YAF16BE:

libswscale/input.c
@@ -2469,15 +2469,19 @@ av_cold void ff_sws_init_input_funcs(SwsInternal *c,
         *alpToYV12 = bswap16Y_c;
         break;
 #endif
+    case AV_PIX_FMT_GRAY10MSBLE:
     case AV_PIX_FMT_YUV444P10MSBLE:
         *lumToYV12 = shf16_10LEToY_c;
         break;
+    case AV_PIX_FMT_GRAY12MSBLE:
     case AV_PIX_FMT_YUV444P12MSBLE:
         *lumToYV12 = shf16_12LEToY_c;
         break;
+    case AV_PIX_FMT_GRAY10MSBBE:
     case AV_PIX_FMT_YUV444P10MSBBE:
         *lumToYV12 = shf16_10BEToY_c;
         break;
+    case AV_PIX_FMT_GRAY12MSBBE:
     case AV_PIX_FMT_YUV444P12MSBBE:
         *lumToYV12 = shf16_12BEToY_c;
         break;

libswscale/swscale_unscaled.c
@@ -2560,6 +2560,8 @@ void ff_get_unscaled_swscale(SwsInternal *c)
         IS_DIFFERENT_ENDIANESS(srcFormat, dstFormat, AV_PIX_FMT_GRAY12) ||
         IS_DIFFERENT_ENDIANESS(srcFormat, dstFormat, AV_PIX_FMT_GRAY14) ||
         IS_DIFFERENT_ENDIANESS(srcFormat, dstFormat, AV_PIX_FMT_GRAY16) ||
+        IS_DIFFERENT_ENDIANESS(srcFormat, dstFormat, AV_PIX_FMT_GRAY10MSB) ||
+        IS_DIFFERENT_ENDIANESS(srcFormat, dstFormat, AV_PIX_FMT_GRAY12MSB) ||
         IS_DIFFERENT_ENDIANESS(srcFormat, dstFormat, AV_PIX_FMT_YA16)   ||
         IS_DIFFERENT_ENDIANESS(srcFormat, dstFormat, AV_PIX_FMT_AYUV64) ||
         IS_DIFFERENT_ENDIANESS(srcFormat, dstFormat, AV_PIX_FMT_GBRP9)  ||

tests/ref/fate/filter-pixdesc-gray10msbbe
@@ -1,0 +1,1 @@
+pixdesc-gray10msbbe 30925ffca4894232df5b2b7d96dcfe54

tests/ref/fate/filter-pixdesc-gray10msble
@@ -1,0 +1,1 @@
+pixdesc-gray10msble 93c7eb73cc0021262ab6fb984789a358

tests/ref/fate/filter-pixdesc-gray12msbbe
@@ -1,0 +1,1 @@
+pixdesc-gray12msbbe 7b74d4e211ec8ff87c94c4a7576dc652

tests/ref/fate/filter-pixdesc-gray12msble
@@ -1,0 +1,1 @@
+pixdesc-gray12msble abc39dc30e8c9f2161e238e85f0db801

tests/ref/fate/filter-pixfmts-copy
@@ -51,8 +51,12 @@ gbrpf32le           f3aff67024db002d8d985c775ec8c235
 gray                188590b1231afd231ea910815aef2b25
 gray10be            0804e8620fcd78599e3df33cb83652aa
 gray10le            705e51fb783ae8167498d09748e44bd2
+gray10msbbe         a4eddf6055d0507c6bc411a788e28c1d
+gray10msble         19c68a45da74b833993a3120fda93ca0
 gray12be            2163197b5975c0c2900ac7c3f56f45f1
 gray12le            5bf2ca7795dd0524b253aee20f660e73
+gray12msbbe         b6a952019c10cc6bd655850c4d226d77
+gray12msble         b46e317ee809b44a25707cea426f2e8e
 gray14be            dccc8bde352b6c8ae65ca2832a383381
 gray14le            56c9e613b09f00a5940c0bf2e938c02e
 gray16be            47a6889ea2dcff9164888a94cccff4de

ffstaging/FFpatches

commit 0627a040188b6cb9735246fd7df2091961cc7f62

swscale/format: add missing fmt_shift for gray12/12 msb formats (PR #20849)
PR #20849 opened by Niklas Haas (haasn)
URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/20849
Patch URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/20849.patch

The MSB YUV formats were added, but the gray formats were not. Seems to have
been an oversight.

Fixes: a5be0ecbfdab24632a10c8a6714844530f76d368

>From c9710dae3c6151a22f0fff60f0a9b4514f6a3b97 Mon Sep 17 00:00:00 2001
From: Niklas Haas <git@haasn.dev>
Date: Thu, 6 Nov 2025 15:56:24 +0100
Subject: [PATCH] swscale/format: add missing fmt_shift for gray12/12 msb
 formats

The MSB YUV formats were added, but the gray formats were not. Seems to have
been an oversight.

Fixes: a5be0ecbfdab24632a10c8a6714844530f76d368

libswscale/format.c
@@ -757,6 +757,8 @@ static int fmt_shift(enum AVPixelFormat fmt)
     case AV_PIX_FMT_YUV444P10MSBLE:
     case AV_PIX_FMT_GBRP10MSBBE:
     case AV_PIX_FMT_GBRP10MSBLE:
+    case AV_PIX_FMT_GRAY10MSBBE:
+    case AV_PIX_FMT_GRAY10MSBLE:
         return 6;
     case AV_PIX_FMT_P012BE:
     case AV_PIX_FMT_P012LE:
@@ -776,6 +778,8 @@ static int fmt_shift(enum AVPixelFormat fmt)
     case AV_PIX_FMT_YUV444P12MSBLE:
     case AV_PIX_FMT_GBRP12MSBBE:
     case AV_PIX_FMT_GBRP12MSBLE:
+    case AV_PIX_FMT_GRAY12MSBBE:
+    case AV_PIX_FMT_GRAY12MSBLE:
         return 4;
     }
 