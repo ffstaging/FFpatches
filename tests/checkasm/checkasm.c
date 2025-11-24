b1526b581e3d30de1b302ed6dcae46cb6e2dfb9b
george.zaguri <code@ffmpeg.org>
2025-11-24 21:03:14 +0000

    configure: add detection of assembler support for SME (PR #21009)
    PR #21009 opened by george.zaguri
    URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/21009
    Patch URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/21009.patch
    
    All changes are made during development/testing of SME for ffmpeg (vvc). Tested on Apple M4.
    
    Additinally fixed bug in Makefile vars to exclude subvars for SVE/SVE2, otherwise it leads to inclusion of SVE/SVE2/SME-specific object files into each ffmpeg target library
    
    >From 652083378193c2a812fab9fa01d1ae0593768858 Mon Sep 17 00:00:00 2001
    From: Georgii Zagoruiko <george.zaguri@gmail.com>
    Date: Mon, 24 Nov 2025 21:01:03 +0000
    Subject: [PATCH] configure: add detection of assembler support for SME
    
    All changes are made during development/testing of SME for ffmpeg (vvc). Tested on Apple M4.
    
    Additinally fixed bug in Makefile vars to exclude subvars for SVE/SVE2, otherwise it leads to inclusion of SVE/SVE2/SME-specific object files into each ffmpeg target library

--- tests/checkasm/checkasm.c
+++ tests/checkasm/checkasm.c
@@ -351,6 +351,7 @@ static const struct {
     { "I8MM",     "i8mm",     AV_CPU_FLAG_I8MM },
     { "SVE",      "sve",      AV_CPU_FLAG_SVE },
     { "SVE2",     "sve2",     AV_CPU_FLAG_SVE2 },
+    { "SME",      "sme",      AV_CPU_FLAG_SME },
 #elif ARCH_ARM
     { "ARMV5TE",  "armv5te",  AV_CPU_FLAG_ARMV5TE },
     { "ARMV6",    "armv6",    AV_CPU_FLAG_ARMV6 },
