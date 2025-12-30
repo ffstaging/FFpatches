6d6122c2d8cb4b73dd48873a56f5ca67b01f3dd4
Marton Balint <code@ffmpeg.org>
2025-12-30 17:49:13 +0000

    avformat/vorbiscomment: fix writing huge chapter numbers and time offets to vorbiscomment (PR #21322)
    PR #21322 opened by Marton Balint (cus)
    URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/21322
    Patch URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/21322.patch
    
    Previous code truncated chapter numbers more than 999 and and time offsets more
    than 99 hours.
    
    The Vorbis chapter extension only allows 1000 chapters, but there is a
    trivial way to extend the syntax, so let's do that instead. Alternate would be
    to reject it or only write the first 1000 chapters.
    
    In any case, it is better than the current code which writes truncated entries.
    
    This also fixes the GCC warnings for truncated strings.
    
    >From 27df50b1b31422191c4ae7f1d174dc3fa3d816ce Mon Sep 17 00:00:00 2001
    From: Marton Balint <cus@passwd.hu>
    Date: Thu, 25 Dec 2025 20:03:32 +0100
    Subject: [PATCH 1/3] avformat/aviobuf: return error for ffio_close_null_buf()
     if written bytes exceed INT_MAX
    
    Also check return value where it is used.
    
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- libavformat/vorbiscomment.c
+++ libavformat/vorbiscomment.c
@@ -20,9 +20,11 @@
  */
 
 #include "avio.h"
+#include "avio_internal.h"
 #include "avformat.h"
 #include "metadata.h"
 #include "vorbiscomment.h"
+#include "libavutil/bprint.h"
 #include "libavutil/dict.h"
 
 /**
@@ -38,27 +40,21 @@ const AVMetadataConv ff_vorbiscomment_metadata_conv[] = {
     { 0 }
 };
 
-int64_t ff_vorbiscomment_length(const AVDictionary *m, const char *vendor_string,
-                                AVChapter **chapters, unsigned int nb_chapters)
+int ff_vorbiscomment_length(const AVDictionary *m, const char *vendor_string,
+                            AVChapter **chapters, unsigned int nb_chapters)
 {
-    int64_t len = 8;
-    len += strlen(vendor_string);
-    if (chapters && nb_chapters) {
-        for (int i = 0; i < nb_chapters; i++) {
-            const AVDictionaryEntry *tag = NULL;
-            len += 4 + 12 + 1 + 10;
-            while ((tag = av_dict_iterate(chapters[i]->metadata, tag))) {
-                int64_t len1 = !strcmp(tag->key, "title") ? 4 : strlen(tag->key);
-                len += 4 + 10 + len1 + 1 + strlen(tag->value);
-            }
-        }
-    }
-    if (m) {
-        const AVDictionaryEntry *tag = NULL;
-        while ((tag = av_dict_iterate(m, tag))) {
-            len += 4 +strlen(tag->key) + 1 + strlen(tag->value);
-        }
-    }
+    AVIOContext *avio_buf;
+    int ret, len;
+
+    ret = ffio_open_null_buf(&avio_buf);
+    if (ret < 0)
+        return ret;
+
+    ret = ff_vorbiscomment_write(avio_buf, m, vendor_string, chapters, nb_chapters);
+    len = ffio_close_null_buf(avio_buf);
+    if (ret < 0)
+        return ret;
+
     return len;
 }
 
@@ -91,8 +87,7 @@ int ff_vorbiscomment_write(AVIOContext *pb, const AVDictionary *m,
         }
         for (int i = 0; i < nb_chapters; i++) {
             AVChapter *chp = chapters[i];
-            char chapter_time[13];
-            char chapter_number[4];
+            AVBPrint bp;
             int h, m, s, ms;
 
             s  = av_rescale(chp->start, chp->time_base.num, chp->time_base.den);
@@ -100,23 +95,21 @@ int ff_vorbiscomment_write(AVIOContext *pb, const AVDictionary *m,
             m  = (s / 60) % 60;
             ms = av_rescale_q(chp->start, chp->time_base, av_make_q(   1, 1000)) % 1000;
             s  = s % 60;
-            snprintf(chapter_number, sizeof(chapter_number), "%03d", i);
-            snprintf(chapter_time, sizeof(chapter_time), "%02d:%02d:%02d.%03d", h, m, s, ms);
-            avio_wl32(pb, 10 + 1 + 12);
-            avio_write(pb, "CHAPTER", 7);
-            avio_write(pb, chapter_number, 3);
-            avio_w8(pb, '=');
-            avio_write(pb, chapter_time, 12);
+            av_bprint_init(&bp, 0, AV_BPRINT_SIZE_AUTOMATIC);
+            av_bprintf(&bp, "CHAPTER%03d=%02d:%02d:%02d.%03d", i, h, m, s, ms);
+            avio_wl32(pb, bp.len);
+            avio_write(pb, bp.str, bp.len);
 
             tag = NULL;
             while ((tag = av_dict_iterate(chapters[i]->metadata, tag))) {
                 int64_t len1 = !strcmp(tag->key, "title") ? 4 : strlen(tag->key);
                 int64_t len2 = strlen(tag->value);
-                if (len1+1+len2+10 > UINT32_MAX)
+                av_bprint_clear(&bp);
+                av_bprintf(&bp, "CHAPTER%03d", i);
+                if (len1+1+len2+bp.len > UINT32_MAX)
                     return AVERROR(EINVAL);
-                avio_wl32(pb, 10 + len1 + 1 + len2);
-                avio_write(pb, "CHAPTER", 7);
-                avio_write(pb, chapter_number, 3);
+                avio_wl32(pb, bp.len + len1 + 1 + len2);
+                avio_printf(pb, bp.str, bp.len);
                 if (!strcmp(tag->key, "title"))
                     avio_write(pb, "NAME", 4);
                 else
