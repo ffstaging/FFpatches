c2d59c97cf36c5e118f1455f1ab0f276a2350007
James Almer <code@ffmpeg.org>
2026-02-11 16:58:36 +0000

    avcodec/hevc/hevcdec: report unparsed unknown NALUs types at verbose level (PR #21735)
    PR #21735 opened by James Almer (jamrial)
    URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/21735
    Patch URL: https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/21735.patch
    
    Reduces spamming at default log level.
    
    Fixes issue #21734
    
    >From f0fcb798739c818ecea2b8bd37d6557797707778 Mon Sep 17 00:00:00 2001
    From: James Almer <jamrial@gmail.com>
    Date: Wed, 11 Feb 2026 13:58:05 -0300
    Subject: [PATCH] avcodec/hevc/hevcdec: report unparsed unknown NALUs types at
     verbose level
    
    Reduces spamming at default log level.
    
    Fixes issue #21734.
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/hevc/hevcdec.c
+++ libavcodec/hevc/hevcdec.c
@@ -3668,7 +3668,7 @@ static int decode_nal_unit(HEVCContext *s, unsigned nal_idx)
     case HEVC_NAL_UNSPEC62:
         break;
     default:
-        av_log(s->avctx, AV_LOG_INFO,
+        av_log(s->avctx, AV_LOG_VERBOSE,
                "Skipping NAL unit %d\n", s->nal_unit_type);
     }
 
